2e190a276874a8fb324fe2487493195247185b15
Deepak <deepak@Deepaks-MacBook-Pro.local>
2017-09-16 12:36:12 +0530

    Added admin auths

--- src/main/java/fab/wallet/backend/controller/UserController.java
+++ src/main/java/fab/wallet/backend/controller/UserController.java
@@ -2,11 +2,9 @@ package fab.wallet.backend.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,7 +20,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "/fab/users", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/fab/authenticate", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Response fetchUsers(@RequestBody Request request, HttpServletRequest httpRequest,
 			HttpServletResponse response) throws Exception {
 		return userService.authenticateUser(request);
